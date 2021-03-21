5cd72692d254a74b2701f5ed0780eacfd77b1372
Austin Berrier <berriera@oregonstate.edu>
2021-03-21 00:01:47 -0700

    Update column_class.py
    - Converted script into class code

--- wec_design_optimization/flexible_column/column_class.py
+++ wec_design_optimization/flexible_column/column_class.py
@@ -1,176 +1,202 @@
 import capytaine as cpt
 import numpy as np
-import logging
-from math import pi, sqrt
-from scipy.integrate import quad
-import matplotlib.pyplot as plt
 
-import os
-os.system('cls')
-logging.basicConfig(level=logging.INFO, format="%(levelname)s:\t%(message)s")
+class FlexibleColumn(object):
 
-# Independent geometry design variables
-radius = 10.0
-height = 200.0
+    def __init__(self, radius, height, elastic_modulus, density, logging=False):
+        from math import pi, sqrt
 
-# Independent material design variables
-elastic_modulus = 4.8e9 / 100 # Pa
-density = 500.0
-nu = 0.3
+        # Constant wave conditions
+        self.water_depth = -height
+        self.wave_periods = np.linspace(3, 20, 50)
+        self.wave_direction = 0.0
 
-# Period definitions
-t_start = 5.0
-t_end = 10.0
+        # Constant beam modal properties
+        self.eigenvalue_list = np.array([1.8751, 4.6941, 7.8458, 10.9955])
+        self.mode_count = 4
 
-# Modal definitions
-#k1l = 1.8751
-k1l = 4.6941
-mode_count = 1
+        # Unpack independent design variables
+        self.radius = radius
+        self.height = height
+        self.elastic_modulus = elastic_modulus
+        self.density = density
 
-# Incident wave frequencies
-wave_periods = np.linspace(t_start, t_end, 30)
-wave_frequencies = 2 * pi / wave_periods
+        # Initialize logger
+        if logging:
+            import logging
+            logging.basicConfig(level=logging.INFO, format="%(levelname)s:\t%(message)s")
 
-# Dependent geometry values
-cross_sectional_area = pi * (radius ** 2)
-volume = cross_sectional_area * height
-mass = density * volume
-inertia = (pi / 4) * (radius ** 4)
+        # Dependent incident wave frequency
+        self.wave_frequencies = 2 * pi / self.wave_periods
 
-# Dependent modal values
-k = k1l / height
-modal_frequency = sqrt(((elastic_modulus * inertia) / (density * cross_sectional_area)) * (k ** 4))
-modal_period = (2 * pi) / modal_frequency
+        # Dependent geometry variables
+        self.cross_sectional_area = pi * (self.radius ** 2)
+        self.volume = self.cross_sectional_area * self.height
+        
+        # Dependent inertia variables
+        self.mass = self.density * self.volume
+        self.area_y_moment_of_inertia = (pi / 4) * (self.radius ** 4)
 
-# Modal properties
-modal_mass = 0.25 * mass
-modal_stiffness = modal_mass * (modal_frequency ** 2)
-water_depth = -height
-
-def bending_mode(x, y, z):
-    from math import cosh, sinh, cos, sin
-
-    q = 1 + z / height
-
-    #k1l = 1.8751
-    k1l = 4.6941
-
-    c1 = (sinh(k1l) - sin(k1l)) / (cosh(k1l) + cos(k1l))
-    end_displacement = cosh(k1l) - cos(k1l) - c1 * (sinh(k1l) - sin(k1l))
-
-    u = (cosh(k1l * q) - cos(k1l * q) - c1 * (sinh(k1l * q) - sin(k1l * q))) / end_displacement
-    v = 0.0
-    w = 0.0
-
-    return (u, v, w)
-
-def integration_mode(q):
-    from math import cosh, sinh, cos, sin
-
-    #k1l = 1.8751
-    k1l = 4.6941
-
-    c1 = (sinh(k1l) - sin(k1l)) / (cosh(k1l) + cos(k1l))
-    end_displacement = cosh(k1l) - cos(k1l) - c1 * (sinh(k1l) - sin(k1l))
-    u = (cosh(k1l * q) - cos(k1l * q) - c1 * (sinh(k1l * q) - sin(k1l * q))) / end_displacement
-
-    return u
-
-def integration_mode_product(q):
-    return integration_mode(q) ** 2
-
-x_plot = np.linspace(0, 1.0, 100)
-y_plot = np.zeros_like(x_plot)
-m = 0
-for x in x_plot:
-    y_plot[m] = integration_mode(x)
-    m += 1
-plt.plot(x_plot, y_plot)
-plt.show()
+        # Dependent modal values
+        self.kappa_list = self.eigenvalue_list / self.height
+        self.modal_frequency_list = sqrt(((self.elastic_modulus * self.area_y_moment_of_inertia) / (self.density * self.cross_sectional_area)) \
+                                    * (self.kappa_list ** 4))
+        self.modal_period_list = (2 * pi) / self.modal_frequency_list
 
 
-modal_mass_coefficient = quad(func=integration_mode_product, a=0.0, b=1.0)[0]
-print(modal_mass_coefficient)
-print(modal_mass_coefficient * mass)
+    def generate_column(self):
+        column = cpt.VerticalCylinder(length=self.height + 2, radius=self.radius, 
+                                        center=(0, 0, -self.height / 2), 
+                                        nx=50, ntheta=30, nr=8,
+                                        clever=False)
+
+        for k in range(self.mode_count):
+                key_name = 'Flexure Mode ' + str(k)
+                column.dofs[key_name] = np.array([self.cantilever_beam_flexure_mode_shape(x, y, z, mode_number=k)
+                                        for x, y, z in column.mesh.faces_centers])    
+
+        column.keep_immersed_part(sea_bottom=self.water_depth)
+
+        column.mass = column.add_dofs_labels_to_matrix(self.mass_matrix())
+        column.dissipation = column.add_dofs_labels_to_matrix(self.damping_matrix())
+        column.hydrostatic_stiffness = column.add_dofs_labels_to_matrix(self.stiffness_matrix())
+
+        self.column_mesh = column
+
+    def cantilever_beam_flexure_mode_shape(self, x, y, z, mode_number):
+        from math import cosh, sinh, cos, sin
+
+        eigenvalue = self.eigenvalue_list[mode_number]
+
+        # Normalize z coordinate value to q from 0 (sea bottom) to 1 (free surface)
+        q = 1 + z / self.height
+
+        c1 = (sinh(eigenvalue) - sin(eigenvalue)) / (cosh(eigenvalue) + cos(eigenvalue))
+        end_displacement = cosh(eigenvalue) - cos(eigenvalue) - c1 * (sinh(eigenvalue) - sin(eigenvalue))
+
+        u = (cosh(eigenvalue*q) - cos(eigenvalue*q) - c1*(sinh(eigenvalue*q) - sin(eigenvalue*q))) / end_displacement
+        v = 0.0
+        w = 0.0
+
+        return (u, v, w)
 
 
-column = cpt.VerticalCylinder(length=height + 2, radius=radius, 
-                                center=(0, 0, -height / 2), 
-                                nx=50, ntheta=30, nr=8,
-                                clever=False)
-column.keep_immersed_part(sea_bottom=water_depth)
-
-column.dofs['Bending 1'] = np.array([bending_mode(x, y, z)
-                                     for x, y, z, in column.mesh.faces_centers])
-
-column.mass = column.add_dofs_labels_to_matrix([[modal_mass]])
-column.hydrostatic_stiffness = column.add_dofs_labels_to_matrix([[modal_stiffness]])
-column.dissipation = column.add_dofs_labels_to_matrix([[0.0]])
-
-animation = column.animate(motion={'Bending 1': 30.0}, loop_duration=8.0)
-animation.run()
-
-wave_direction = 0.0
-bem_solver = cpt.BEMSolver()
-problems = [cpt.RadiationProblem(sea_bottom=water_depth, body=column, 
-            radiating_dof=dof, omega=omega) 
-            for dof in column.dofs for omega in wave_frequencies]
-problems += [cpt.DiffractionProblem(sea_bottom=water_depth, body=column, 
-            wave_direction=wave_direction, omega=omega) 
-            for omega in wave_frequencies]
-
-results = [bem_solver.solve(problem) for problem in problems]
-data = cpt.assemble_dataset(results)
-
-data['RAO'] = cpt.post_pro.rao(data, wave_direction=wave_direction)
+    def mass_matrix(self):
+        modal_mass = 0.25 * self.mass
+        mass_matrix = modal_mass * np.ones(shape = self.mode_count)
+        mass_matrix = np.diag(mass_matrix)
+        
+        return mass_matrix
 
 
-plt.figure()
-for dof in column.dofs:
-    plt.plot(
-        wave_periods,
-        data['added_mass'].sel(radiating_dof=dof, influenced_dof=dof),
-        label=dof,
-        marker='o'
-    )
-plt.xlabel('T (s)')
-plt.ylabel('Added Mass')
-plt.ylim((0, 1.6e7))
-plt.legend()
-plt.tight_layout()
-#plt.show()
-plt.savefig('added_mass.png', bbox_inches='tight')
+    def damping_matrix(self):
+        damping_matrix = np.zeros(shape=(self.mode_count, self.mode_count))
 
-plt.figure()
-for dof in column.dofs:
-    plt.plot(
-        wave_periods,
-        data['radiation_damping'].sel(radiating_dof=dof, influenced_dof=dof),
-        label=dof,
-        marker='o'
-    )
-#plt.xlabel('$\omega \$ (rad/s)')
-plt.xlabel('T (s)')
-plt.ylabel('Radiation damping')
-plt.ylim((0, 5e6))
-plt.legend()
-#plt.tight_layout()
-#plt.show()
-plt.savefig('radiation_damping.png', bbox_inches='tight')
+        return damping_matrix
 
-plt.figure()
-for dof in column.dofs:
-    plt.plot(
-        wave_periods, 
-        np.abs(data['RAO'].sel(radiating_dof=dof).data),
-        label=dof,
-        marker='o'
-    )
-#plt.xlabel('$\omega \ $ (rad/s)')
-plt.xlabel('T (s)')
-plt.ylabel('RAO')
-#plt.ylim((0, 2.0))
-plt.legend()
-plt.tight_layout()
-plt.show()
-#plt.savefig('response_amplitude_operator.png', bbox_inches='tight')
+
+    def stiffness_matrix(self):
+        modal_mass = 0.25 * self.mass
+        stiffness_matrix = modal_mass * np.diag(self.modal_frequency_list ** 2)
+
+        return stiffness_matrix
+
+    def evaluate_column_modal_response_amplitudes(self):
+        """
+
+        """
+        bem_solver = cpt.BEMSolver()
+        problems = [cpt.RadiationProblem(sea_bottom=self.water_depth, body=self.column_mesh, 
+                                        radiating_dof=dof, omega=omega)
+                                        for dof in self.column_mesh.dofs for omega in self.wave_frequencies]
+        problems += [cpt.DiffractionProblem(sea_bottom=self.water_depth, body=self.column_mesh,
+                                        wave_direction=self.wave_direction, omega=omega)
+                                        for omega in self.wave_frequencies]
+        results = [bem_solver.solve(problem) for problem in problems]
+        result_data = cpt.assemble_dataset(results)
+        modal_response_amplitude_data = cpt.post_pro.rao(result_data, wave_direction=self.wave_direction)
+
+        self.result_data = result_data
+        self.modal_response_amplitude_data = modal_response_amplitude_data
+
+    def plot_mode_shapes(self):
+        import matplotlib.pyplot as plt
+        from math import nan
+
+        plt.figure()
+        for mode_number in range(self.mode_count):
+            column_z = np.linspace(0, 1, 250)
+            column_deformation = np.zeros_like(column_z)
+
+            k = 0
+            for z in column_z:
+                column_deformation[k] = self.cantilever_beam_flexure_mode_shape(x=nan, y=nan, z=z, mode_number=mode_number, plot_flag=True)
+                k += 1
+            plt.plot(column_z, column_deformation, label='Mode {} ($\omega = {:.3f}$ rad/s)'
+                    .format(mode_number, self.modal_period_list[mode_number]))
+        
+        plt.xlabel('$z (m)$')
+        plt.ylabel('$\delta u (m)$')
+        plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+        plt.savefig('mode_shapes.png', bbox_inches='tight')
+
+        return
+
+    def save_hydrodynamic_result_figures(self):
+        import matplotlib.pyplot as plt
+
+        plt.figure()
+        for dof in self.column_mesh.dofs:
+            plt.plot(
+                self.wave_periods,
+                self.result_data['added_mass'].sel(radiating_dof=dof, influenced_dof=dof),
+                label=dof,
+                marker='o'
+            )
+        plt.xlabel('T (s)')
+        plt.ylabel('Added Mass')
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+        plt.savefig('added_mass.png', bbox_inches='tight')
+
+        plt.figure()
+        for dof in self.column_mesh.dofs:
+            plt.plot(
+                self.wave_periods,
+                self.result_data['radiation_damping'].sel(radiating_dof=dof, influenced_dof=dof),
+                label=dof,
+                marker='o'
+            )
+        plt.xlabel('T (s)')
+        plt.ylabel('Radiation damping')
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+        plt.savefig('radiation_damping.png', bbox_inches='tight')
+
+        plt.figure()
+        for dof in self.column_mesh.dofs:
+            plt.plot(
+                self.wave_periods, 
+                np.abs(self.result_data['RAO'].sel(radiating_dof=dof).data),
+                label=dof,
+                marker='o'
+            )
+        plt.xlabel('T (s)')
+        plt.ylabel('RAO')
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+        plt.savefig('response_amplitude_operators.png', bbox_inches='tight')
+
+    def animate_column(self, wave_period):
+        motion_dict = {}
+        for dof in self.column_mesh.dofs:
+            motion_dict[dof] = self.modal_response_amplitude_data  
+            # TODO: go off nearest wave frequency or interpolate between two neighbors
+        animation = self.column_mesh.animate(motion=motion_dict, loop_duration=wave_period)
+        animation.run()
+
+
+
