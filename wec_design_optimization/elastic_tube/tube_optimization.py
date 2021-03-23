ad0052f1476b8167a057d7d4ffef981853f50778
Austin Berrier <berriera@oregonstate.edu>
2021-03-22 19:37:50 -0700

    Update tube_optimization.py

--- wec_design_optimization/elastic_tube/tube_optimization.py
+++ wec_design_optimization/elastic_tube/tube_optimization.py
@@ -6,7 +6,9 @@ from tube_class import evaluate_tube_design
 #   length 
 #   submergence
 #   power_take_off_damping
+#   wall_stiffness
+#   fiber_pretension
 
-design_variables = np.array([0.9, 60, -1.35, 120e3])
+design_variables = np.array([0.9, 60, -1.35, 30e3])
 tube_objective = evaluate_tube_design(design_variables)
 print(tube_objective)
