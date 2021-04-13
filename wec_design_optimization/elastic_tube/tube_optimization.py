14ede42bab9d29ccbc940caaf265a91b137a2bc1
Austin Berrier <berriera@oregonstate.edu>
2021-04-13 00:25:50 -0700

    Update tube_optimization.py
    - Removed PTO damping variable from array
    - Started on pattern search

--- wec_design_optimization/elastic_tube/tube_optimization.py
+++ wec_design_optimization/elastic_tube/tube_optimization.py
@@ -9,6 +9,11 @@ from tube_class import evaluate_tube_design
 #   wall_stiffness
 #   fiber_pretension
 
-design_variables = np.array([0.9, 60, -1.35, 30e3])
+design_variables = np.array([0.9, 60, -1.35])
 tube_objective = evaluate_tube_design(design_variables)
 print(tube_objective)
+
+# TODO: potentially specify optimization type as a string: 'geometry', 'material', 'all' to pick the variables
+
+moves_list = []
+converged = False
