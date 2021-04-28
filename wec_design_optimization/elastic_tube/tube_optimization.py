ef129476cf0f071eff8f0dae1380c345b0b40d5c
Austin Berrier <berriera@oregonstate.edu>
2021-04-27 19:05:26 -0700

    Update tube_optimization.py
    - Adjusted test objective function for more refined global solution
    - Added optimization logger to output to .txt file
    - Added history checker to lower needed iteration requirements for repeated design evaluations

--- wec_design_optimization/elastic_tube/tube_optimization.py
+++ wec_design_optimization/elastic_tube/tube_optimization.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pickle
+import datetime
 from tube_class import evaluate_tube_design
 
-# Test objective function with global minimum at (5, 30, -10)
+# Test objective function with global minimum at (2.15, 32.5, -8.75)
 def obj_func(x):
-    return (x[0] - 5.0) ** 2 + (x[1] - 30.0) ** 2 + (x[2] + 10) ** 2
+    return (x[0] - 2.15) ** 2 + (x[1] - 32.5) ** 2 + (x[2] + 8.75) ** 2
 
 # Design variables in order: 
 #   static_radius r_s
@@ -40,10 +41,23 @@ function_history = []
 converged = False
 iteration_count = 1
 
+# Print log file header
+f = open('optimization_history_file.txt', 'w')
+print('Geometry optimization run, 5 modes:', file=f)
+print(datetime.datetime.today(), file=f)
+variables = '\nVariables:\n' + '\tStatic radius r_s' + '\tLength L' + '\tSubmergence z_s'
+bounds = 'Bounds:\n' + '\tr_s   in [0.05, 2.5]\n' + '\tL in [20.0, 200.0]\n' + '\tz_s in [-25.0, 2.5]'
+print(variables, file=f)
+print(bounds, file=f)
+print('\nMoves list: ', file=f)
+print(moves_list, file=f)
+
+
 # Set up first history values
 location_history.append(current_location)
 function_history.append(current_objective_value)
-print('Objective function value = {:.2f}'.format(current_objective_value))
+print('\nFirst design is {}'.format(current_location), file=f)
+print('Objective function value = {:.2f}'.format(current_objective_value), file=f)
 
 # Initialize potential move counters
 move_number = 0
@@ -62,37 +76,35 @@ def check_bounds(potential_design):
     return np.all(potential_design <= upper_bounds) and np.all(potential_design >= lower_bounds)
 
 
-def check_constraints(potential_design):
-    from math import fabs
-    static_radius = potential_design[0]
-    submergence = potential_design[2]
-
-    return submergence < -static_radius
-
+def check_history(history_list, potential_design):
+    for design in history_list:
+        if np.array_equal(potential_design, design):
+            return True
+    return False
 
 while not converged:
     # Pick new move
     movement = move_direction * move_multiplier * moves_list[move_number]
-    print('\nMoving in direction: ', movement)
+    print('\nMoving in direction: {}'.format(movement), file=f)
 
     # Update design variables
     new_location = current_location + movement
+    print('\tTo test design variables: {}'.format(new_location), file=f)
 
     # Check bounds first and constraint if relevant
     bounds_check = check_bounds(new_location)
-    #constraint_check = check_constraints(new_location)
-
-    if new_location in location_history:
-        location_index = np.where(new_location == location_history)[0][0]   # Check
-        new_objective_value = function_history[location_index]
+    if check_history(location_history, new_location):
+        print('\tDesign {} already visited'.format(new_location), file=f)
+        past_location_index = np.where(new_location == location_history)[0][0]
+        new_objective_value = function_history[past_location_index]
         
 
-    # Try new move if in bounds
+    # Try new move if in bounds and has not been visited before
     elif bounds_check:
         iteration_count = iteration_count + 1
 
-        print('New design is in bounds. Evaluating iteration number: ', iteration_count)
-        print('\n\tWith design variables: ', new_location)
+        print('\tNew design is in bounds. Evaluating iteration number: {}'.format(iteration_count), file=f)
+        print('Evaluating design {} with location {} !'.format(iteration_count, new_location))
 
         new_objective_value = evaluate_tube_design(new_location)
         #new_objective_value = obj_func(new_location)
@@ -100,24 +112,26 @@ while not converged:
         # Update histories
         location_history.append(new_location)
         function_history.append(new_objective_value)
-        print('\n\tObjective function value: ', new_objective_value)
+        print('\tObjective function value: {}'.format(new_objective_value), file=f)
 
     else:
+        print('\tNew design not in bounds. Trying next move.', file=f)
         new_objective_value = 1e23
 
     # Update design if tested one is better
     if new_objective_value < current_objective_value:
-        print('Moving to new location')
+        print('\tMoving to new location', file=f)
         current_location = np.copy(new_location)
         current_objective_value = new_objective_value
 
     # Tries a different move if the new objective function value is worse than the current one.
-    # This will happen if the new location is out of bounds, does not satisfy the constraint, or
-    # if the returned power value is worse.
+    # This will happen if the new location is out of bounds or if the returned power value is worse.
+
+    # Reverses the search direction
     elif move_direction == 1:
         move_direction = -1
 
-    # Pick next variable to move if both move directions have been worse
+    # Pick next variable to move if both search directions along the current variable have been worse
     elif move_number < (variable_count - 1):
         move_number = move_number + 1
         move_direction = 1
@@ -130,10 +144,10 @@ while not converged:
 
     # Converged if multipler is now equal to 1 for all variables
     else:
-        print('\n\n\nOptimization converged!')
-        print('Final converged design variables: ', current_location)
-        print('\nFinal converged objective function value: ', current_objective_value)
-        print('Iteration count: ', iteration_count)
+        print('\n\n\nOptimization converged!', file=f)
+        print('Final converged design variables: {}'.format(current_location), file=f)
+        print('\nFinal converged objective function value: {}'.format(current_objective_value), file=f)
+        print('Iteration count: {}'.format(iteration_count), file=f)
 
         # Save results
         with open('tube_history.pkl', 'wb') as history_file:
@@ -141,3 +155,5 @@ while not converged:
             pickle.dump(function_history, history_file)
 
         converged = True
+
+f.close()
