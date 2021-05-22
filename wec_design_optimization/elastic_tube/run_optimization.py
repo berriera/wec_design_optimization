6dbbb0675311b3feccd5b96b5ed562c3e0763fce
Austin Berrier <berriera@oregonstate.edu>
2021-05-21 17:28:04 -0700

    Update run_optimization.py
    - Added 10 initial starting points with latin cube sampling
    - Added a small term to the bounds in either direction for allowing points on the bounds to be run
    - Optimization now runs 10 times for the 10 different starting points

--- wec_design_optimization/elastic_tube/run_optimization.py
+++ wec_design_optimization/elastic_tube/run_optimization.py
@@ -13,13 +13,16 @@ def test_quadratic_function(x):
 #   length L
 #   submergence z_s
 
-start_location = np.array([0.9, 60, -1.25])
+starting_locations = [  np.array([ 2.50,  30.0,  2.25]), np.array([ 0.45,  50.0, -8.75]), np.array([ 1.60, 170.0, -11.25]), 
+                        np.array([ 0.30, 114.0, -2.25]), np.array([ 2.30,  74.0,  1.00]), np.array([ 0.70,  94.0,  -3.00]),
+                        np.array([ 3.00, 160.0, -7.00]), np.array([ 1.25, 194.0, -0.75]), np.array([ 0.95, 136.0, -10.50]), 
+                        np.array([ 2.10,  60.0, -5.00])]
 moves_list = [0.05, 2.0, 0.25]
-upper_bounds = np.array([3.00, 200.0,   3.0])
-lower_bounds = np.array([0.05,  20.0, -12.0])
-history_file = 'geometry_optimization__x0=rs_le_zs=[{}]'.format(start_location)
+upper_bounds = np.array([3.00, 200.0,   3.0]) + 1e-4
+lower_bounds = np.array([0.05,  20.0, -12.0]) - 1e-4
 
 header = 'Geometry optimization run, 10 modes, 50 wave frequencies:'
+method = '\nAlgorithm: Greedy hooke and jeeves with dynamic move ordering'
 variables = '\nVariables:\n' + '\tStatic radius rs' + '\tLength le' + '\tSubmergence zs'
 bounds = '\nBounds:\n' + '\tr_s   in [0.05, 3.0]\n' + '\tL in [20.0, 200.0]\n' + '\tz_s in [-12.0, 3.0]\n'
 moves = '\nMoves list: {}'.format(moves_list)
@@ -27,6 +30,8 @@ start_time = '\nStart_time: {}.'.format(datetime.datetime.today())
 
 header = header + variables + bounds + moves + start_time
 
-hooke_jeeves__greedy(obj_function=test_quadratic_function,
-                        starting_point=start_location, upper_bounds=upper_bounds, lower_bounds=lower_bounds,
-                        moves_list=moves_list, history_file=history_file, header=header)
+for starting_design in starting_locations:
+    history_file = 'geometry_optimization__x0=rs_le_zs=[{}]'.format(starting_design)
+    hooke_jeeves__greedy(obj_function=test_quadratic_function,
+                            starting_point=starting_design, upper_bounds=upper_bounds, lower_bounds=lower_bounds,
+                            moves_list=moves_list, history_file=history_file, header=header)
